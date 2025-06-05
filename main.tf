e6fbff8ed7e1d4928265cbb75cffa46ff50cc6cb
pscott12 <paysonscott01@gmail.com>
2025-06-05 15:17:29 -0400

    Adding Storage Account with Container

--- main.tf
+++ main.tf
@@ -3,3 +3,24 @@ resource "azurerm_resource_group" "main" {
   location = var.primary_location
 
 }
+
+resource "random_string" "suffix" {
+  length  = 10
+  special = false
+  upper   = false
+}
+
+resource "azurerm_storage_account" "main" {
+  name                     = "st${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+}
+
+resource "azurerm_storage_container" "GA-DEMO-STORAGE" {
+  name                  = "StorageContainer"
+  storage_account_id    = azurerm_storage_account.main.id
+  container_access_type = "private"
+}
