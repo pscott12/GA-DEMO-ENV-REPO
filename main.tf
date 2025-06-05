28c1a6f6a554c605b417cc5ebbc6e1883735bc22
pscott12 <paysonscott01@gmail.com>
2025-06-05 15:39:23 -0400

    Fix Tenant ID Value

--- main.tf
+++ main.tf
@@ -44,6 +44,6 @@ resource "azurerm_key_vault" "main" {
   name                = "kv-${var.application_name}-${var.enviroment_name}-${random_string.keyvault_suffix.result}"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
+  tenant_id           = ARM_CLIENT_TENANT_ID
   sku_name            = "standard"
 }
