9542cc8d00d63bf6c383875734d52acace6c8e59
pscott12 <paysonscott01@gmail.com>
2025-06-11 13:42:32 -0400

    Creation of NSG

--- network.tf
+++ network.tf
@@ -50,3 +50,22 @@ resource "azurerm_subnet" "delta" {
   address_prefixes     = [local.delta_address_space]
 }
 
+resource "azurerm_network_security_group" "remote_access" {
+  name                = "nsg-${var.application_name_two}-${var.enviroment_name}-remote-access"
+  location            = azurerm_resource_group.network.location
+  resource_group_name = azurerm_resource_group.network.name
+
+  security_rule {
+    name                       = "ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+
+}
