b9df60c910db3664ea46ba202b98064324eb2f4d
pscott12 <paysonscott01@gmail.com>
2025-06-10 22:21:12 -0400

    changing to ip address range

--- network.tf
+++ network.tf
@@ -9,5 +9,5 @@ resource "azurerm_virtual_network" "network" {
   name                = "vnet-${var.application_name_two}-${var.enviroment_name}"
   location            = azurerm_resource_group.network.location
   resource_group_name = azurerm_resource_group.network.name
-  address_space       = ["10.0.0.0/22"]
+  address_space       = ["10.108.0.0/22"]
 }
