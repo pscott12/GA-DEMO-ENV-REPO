e776bcbbd954e7268912f1645ea7c8306629ec73
pscott12 <paysonscott01@gmail.com>
2025-06-10 21:53:45 -0400

    Added Network RG

--- Modules/Networking/main.tf
+++ Modules/Networking/main.tf
@@ -1,1 +1,6 @@
+resource "azurerm_resource_group" "main" {
+  name     = "rg-${var.application_name}-${var.enviroment_name}"
+  location = var.primary_location
 
+
+}
--- Modules/Networking/terraform.tfvars
+++ Modules/Networking/terraform.tfvars
@@ -1,5 +1,5 @@
 enviroment_name  = "dev"
 primary_location = "East US"
-application_name = "GADemo"
+application_name = "network"
 
 
