58b57edb485e5d21ece603a3ad54d6665f66f933
pscott12 <paysonscott01@gmail.com>
2025-06-10 22:18:56 -0400

    adding vnet

--- network.tf
+++ network.tf
@@ -3,3 +3,11 @@ resource "azurerm_resource_group" "network" {
   name     = "rg-${var.application_name_two}-${var.enviroment_name}"
   location = var.primary_location
 }
+
+
+resource "azurerm_virtual_network" "network" {
+  name                = "vnet-${var.application_name_two}-${var.enviroment_name}"
+  location            = azurerm_resource_group.network.location
+  resource_group_name = azurerm_resource_group.network.name
+  address_space       = ["10.0.0.0/22"]
+}
