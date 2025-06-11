838e0e93e0b79a2005844d8c10e38fe2c32c5529
pscott12 <paysonscott01@gmail.com>
2025-06-10 22:07:35 -0400

    name fix for network rg

--- network.tf
+++ network.tf
@@ -1,4 +1,5 @@
-resource "azurerm_resource_group" "main" {
+resource "azurerm_resource_group" "network" {
+  # This resource group is for the network resources
   name     = "rg-${var.application_name}-${var.enviroment_name}"
   location = var.primary_location
 }
