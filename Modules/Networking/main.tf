18d3be6115efebd6e1e777f1f0e655c0fd7034bf
pscott12 <paysonscott01@gmail.com>
2025-06-10 22:10:36 -0400

    deletion of modules dir

--- Modules/Networking/main.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_resource_group" "main" {
-  name     = "rg-${var.application_name}-${var.enviroment_name}"
-  location = var.primary_location
-
-
-}
--- Modules/Networking/terraform.tfvars
+++
@@ -1,5 +0,0 @@
-enviroment_name  = "dev"
-primary_location = "East US"
-application_name = "network"
-
-
