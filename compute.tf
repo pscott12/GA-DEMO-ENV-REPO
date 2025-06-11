bb52dac00cb3e30df5c9aff10ea3f365ec010794
pscott12 <paysonscott01@gmail.com>
2025-06-11 15:10:41 -0400

    Adding public ip address

--- compute.tf
+++ compute.tf
@@ -1,5 +1,12 @@
-resource "azurerm_resource_group" "network" {
+resource "azurerm_resource_group" "linuxvm" {
   # This resource group is for the linuxvm 
   name     = "rg-${var.application_name_three}-${var.enviroment_name}"
   location = var.primary_location
+}
+
+resource "azurerm_public_ip" "vm1" {
+  name                = "pip-${var.application_name_three}-${var.enviroment_name}-vm1"
+  resource_group_name = azurerm_resource_group.linuxvm.name
+  location            = azurerm_resource_group.linuxvm.location
+  allocation_method   = "Static"
 }
