7ef0ca3868fbf755907cd1a294c6e7469adf285d
pscott12 <paysonscott01@gmail.com>
2025-06-12 10:48:45 -0400

    adding public and private dir for keys

--- compute.tf
+++ compute.tf
@@ -28,6 +28,16 @@ resource "tls_private_key" "vm1" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
+resource "local_file" "private_key" {
+  content         = tls_private_key.vm1.private_key_pem
+  filename        = pathexpand("~/.ssh/vm1")
+  file_permission = "0600"
+}
+
+resource "local_file" "public_key" {
+  content  = tls_private_key.vm1.public_key_openssh
+  filename = pathexpand("~/.ssh/vm1.pub")
+}
 
 # Linux VM
 resource "azurerm_linux_virtual_machine" "vm1" {
