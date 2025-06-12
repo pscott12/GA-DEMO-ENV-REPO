d6b7cf1fddae68637a730d7c25da6ca1c918d734
pscott12 <paysonscott01@gmail.com>
2025-06-12 09:59:03 -0400

    Linux VM Creation

--- compute.tf
+++ compute.tf
@@ -28,3 +28,32 @@ resource "tls_private_key" "vm1" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
+
+# Linux VM
+resource "azurerm_linux_virtual_machine" "vm1" {
+  name                = "vm1${var.application_name_three}${var.enviroment_name}"
+  resource_group_name = azurerm_resource_group.linuxvm.name
+  location            = azurerm_resource_group.linuxvm.location
+  size                = "Standard_D2_v2_Promo"
+  admin_username      = "admin"
+  network_interface_ids = [
+    azurerm_network_interface.vm1.id,
+  ]
+
+  admin_ssh_key {
+    username   = "admin"
+    public_key = tls_private_key.vm1.public_key_openssh
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
