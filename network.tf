55787bf9423c1b40067a0c59b7e6f3f8dd2abf04
pscott12 <paysonscott01@gmail.com>
2025-07-09 16:01:44 -0400

    deletion of bastion host due to $$

--- network.tf
+++ network.tf
@@ -65,14 +65,3 @@ resource "azurerm_public_ip" "bastion" {
   sku                 = "Standard"
 }
 
-resource "azurerm_bastion_host" "main" {
-  name                = "bas-${var.application_name_two}-${var.enviroment_name}"
-  location            = azurerm_resource_group.network.location
-  resource_group_name = azurerm_resource_group.network.name
-
-  ip_configuration {
-    name                 = "configuration"
-    subnet_id            = azurerm_subnet.bastion.id
-    public_ip_address_id = azurerm_public_ip.bastion.id
-  }
-}
